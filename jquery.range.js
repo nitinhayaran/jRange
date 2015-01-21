9d23b091fe36b477dc3d3f9090d0f20d7cb38723
Nitin Hayaran <nitinhayaran@gmail.com>
2015-01-21 15:04:44 +0530

    Issue when dragging from left to right
    fix for #10

--- jquery.range.js
+++ jquery.range.js
@@ -33,7 +33,9 @@
 			step  : 1,
 			format: '%s',
 			theme : 'theme-green',
-			width : 300
+			width : 300,
+			minRange: 0,
+			maxRange: 'auto'
 		},
 		template : '<div class="slider-container">\
 			<div class="back-bar">\
@@ -103,7 +105,8 @@
 			if(e.which !== 1){return;}
 			e.stopPropagation(); e.preventDefault();
 			var pointer = $(e.target);
-			pointer.addClass('focused');
+			this.pointers.removeClass('last-active');
+			pointer.addClass('focused last-active');
 			this[(pointer.hasClass('low')?'low':'high') + 'Label'].addClass('focused');
 			$(document).on('mousemove.slider', $.proxy(this.onDrag, this, pointer));
 			$(document).on('mouseup.slider', $.proxy(this.onDragEnd, this));
